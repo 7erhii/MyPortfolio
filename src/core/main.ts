c62d8d9a59094951da8630935e25a5a00010eb8c
7erhiy <7erhiy@gmail.com>
2023-07-05 14:41:35 +0300

    adding js

--- src/core/main.ts
+++ src/core/main.ts
@@ -1,25 +1,46 @@
-// console.log(1);
+const buttons = document.querySelectorAll(".about-btn-js");
+const btnsContainer = document.querySelector(".about__btns") as HTMLElement;
+const descrContainer = document.querySelector(".about__descr") as HTMLElement;
+const descrItems = document.querySelectorAll(".about__descr-item");
+const skillsContainer = document.querySelector(".about__skills") as HTMLElement;
+const descrAll = document.querySelector(".about__descr-item--full");
+const closeAllDesrcBtn = document.querySelector(".about__descr-item-close");
 
-// document.querySelector('.themetoggle').addEventListener('click', (event) => {
-//    event.preventDefault();
-//    if (localStorage.getItem('theme') === 'dark') {
-//       localStorage.removeItem('theme');
-//    }
-//    else {
-//       localStorage.setItem('theme', 'dark')
-//    }
-//    addDarkClassToHTML()
-// });
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    if (index < buttons.length - 1) {
+      descrAll.classList.remove("showed-grid");
 
-// function addDarkClassToHTML() {
-//    try {
-//       if (localStorage.getItem('theme') === 'dark') {
-//          document.querySelector('.page').classList.add('dark');
-//       }
-//       else {
-//          document.querySelector('.page').classList.remove('dark');
-//       }
-//    } catch (err) { }
-// }
+      console.log(`Нажата кнопка с индексом ${index}`);
 
-// addDarkClassToHTML();
+      const offsetY = skillsContainer.offsetHeight;
+
+      btnsContainer.style.transition = "transform 0.3s ease";
+      descrContainer.style.transition = "transform 0.3s ease";
+
+      btnsContainer.style.transform = `translateY(-${offsetY}px)`;
+      descrContainer.style.transform = `translateY(-${offsetY}px)`;
+
+      setTimeout(() => {
+        btnsContainer.style.transform = "";
+        descrContainer.style.transform = "";
+      }, 4000);
+    }
+    if (index === 3) {
+      descrAll.classList.toggle("showed-grid");
+    }
+
+    descrItems.forEach((item, itemIndex) => {
+      if (itemIndex === index) {
+        item.classList.add("showed");
+      } else {
+        item.classList.remove("showed");
+      }
+    });
+  });
+});
+
+closeAllDesrcBtn.addEventListener("click", () => {
+  descrAll.classList.remove("showed-grid");
+});
+
